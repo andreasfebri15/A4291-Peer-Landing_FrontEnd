c183c35fbab0e08cb03284e9139ddce45cc1108f
Andreas Febri Hermawan <83470681+andreasfebri15@users.noreply.github.com>
2024-09-27 07:54:57 +0700

    Add project files.

---
+++ PeerLandingFE/Controllers/Mst_User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeerLandingFE.Controllers
+{
+    public class Mst_User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PeerLandingFE/DTO/Res/ResMstUser.cs
@@ -0,0 +1,15 @@
+﻿namespace PeerLandingFE.DTO.Res
+{
+    public class ResMstUser
+    {
+        public class ResMstUserDto
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string role { get; set; }
+            public string jwtToken { get; set; }
+
+        }
+    }
+}
